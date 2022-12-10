82ca0614b4cf8ff0d3142d576ecac523a5a88047
kayz <kayz@pop-os.localdomain>
2022-12-11 00:17:44 +0100

    working on hashtabel

--- data_structf/hash/hashTables.js
+++ data_structf/hash/hashTables.js
@@ -1,6 +1,7 @@
 /**
  * * Hash tables
- *
+ * * Collision
+ *  two strategies: Separate Chaining or Linear Probing
  */
 
 /**
@@ -26,3 +27,46 @@ const hash2 = (value, arrayLen) => {
     return (hash * WEIRD_PRIME) % arrayLen
 }
 
+class HashTable {
+    constructor(size = 53) {
+        this.keyMap = new Array(size)
+    }
+
+    _hash(key) {
+        let hash = 0
+        let WEIRD_PRIME = 31
+        for (let i = 0; i < Math.min(key.length, 100); i++) {
+            hash += key.charCodeAt(i)
+        }
+        return (hash * WEIRD_PRIME) % this.keyMap.length
+    }
+
+    set(key, value) {
+        let index = this._hash(key)
+        if (!this.keyMap[index]) {
+            this.keyMap[index] = []
+        }
+        this.keyMap[index].push([key, value])
+    }
+
+    get(key) {
+        let index = this._hash(key)
+        if (this.keyMap[index]) {
+            for (let i = 0; i < this.keyMap[index].length; i++) {
+                if (this.keyMap[index][i][0] === key) {
+                    return this.keyMap[index][i][1]
+                }
+            }
+        }
+        return undefined
+    }
+}
+
+let myhash = new HashTable()
+console.log(myhash.set('first', 100))
+console.log(myhash.set('third', 10012))
+console.log(myhash.set('second', 1001))
+console.log(myhash.get('first'))
+console.log(myhash.get('second'))
+
+// console.log(myhash)
