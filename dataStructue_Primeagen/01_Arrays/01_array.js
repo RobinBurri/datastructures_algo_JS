bc11dd29ced627e9f13b5aacc9bbad20373eda75
Robin Burri <robinburri@tutanota.com>
2023-12-13 23:50:28 +0100

    primeagean class started

---
+++ dataStructue_Primeagen/01_Arrays/01_array.js
@@ -0,0 +1,3 @@
+"use strict";
+const a = new ArrayBuffer(6);
+console.log(a);
---
+++ dataStructue_Primeagen/Main.js
@@ -0,0 +1,14 @@
+import linear_search from "./Search/01_linear.js";
+import binarySearch from "./Search/02_binary.js";
+console.log(linear_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 1));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 3));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 4));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 5));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 6));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 7));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 8));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 9));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 10));
+console.log(binarySearch([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11));
